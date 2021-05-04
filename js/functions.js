9449108c73aed453dee1e6c4c351c18f37d72335
Diego-rosas <diegorosas19@gmail.com>
2021-05-03 22:29:18 -0300

    Parte 3: Criando o function.js

---
+++ js/functions.js
@@ -0,0 +1,33 @@
+var engine = {
+    "cores": ['green', 'purple', 'pink', 'red', 'yellow', 'orange', 'grey', 'black'],
+    "hexadecimais":{
+        'green': '#02EF00',
+        'purple': '#790093',
+        'pink': '#F02A7E',
+        'red': '#E90808',
+        'yellow': '#E7D703',
+        'orange': '#F16529', 
+        'grey': '#EBEBEB',
+        'black': '#141414',
+    },
+    "moedas": 0
+}
+
+function sortearCor(){
+    var indexCorSorteada = Math.floor(Math.random() * engine.cores.length);
+    var legendaCorDaCaixa = document.getElementById('cor-na-caixa'); 
+    var nomeCorSorteada = engine.cores[indexCorSorteada];
+
+    legendaCorDaCaixa.innerText = nomeCorSorteada.toUpperCase();
+    
+    return engine.hexadecimais[nomeCorSorteada];
+}
+
+function aplicarCorNaCaixa(nomeDaCor) {
+    var caixaDasCores = document.getElementById('cor-atual');
+
+    caixaDasCores.style.backgroundColor = nomeDaCor; 
+    caixaDasCores.style.backgroundImage = "url('../img/caixa-fechada.png')";
+    caixaDasCores.style.backgroundSize = "100%";
+
+}
